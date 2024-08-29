3f07dc0f0b6446f8709b9a48afc97ca181254091
t13max-x <t13max@163.com>
2024-08-29 11:46:33 +0800

    feat: 操作系统工具类

---
+++ common-util/src/main/java/com/t13max/util/OSUtils.java
@@ -0,0 +1,42 @@
+package com.t13max.util;
+
+/**
+ * 系统工具类 判断当前是什么操作系统
+ *
+ * @author t13max
+ * @since 11:16 2024/8/29
+ */
+public class OSUtils {
+
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return (OS.contains("win"));
+    }
+
+    public static boolean isMac() {
+        return (OS.contains("mac"));
+    }
+
+    public static boolean isUnix() {
+        return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
+    }
+
+    public static boolean isSolaris() {
+        return (OS.contains("sunos"));
+    }
+
+    public static void main(String[] args) {
+        if (isWindows()) {
+            System.out.println("This is Windows");
+        } else if (isMac()) {
+            System.out.println("This is macOS");
+        } else if (isUnix()) {
+            System.out.println("This is Unix or Linux");
+        } else if (isSolaris()) {
+            System.out.println("This is Solaris");
+        } else {
+            System.out.println("Unknown OS");
+        }
+    }
+}
